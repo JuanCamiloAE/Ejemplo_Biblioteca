a3053f7772a50f337bb8129026704156c8588542
Juan Camilo A <juancamiloagudeloe@gmail.com>
2020-10-06 03:42:21 -0500

    Se actualizo el Laravel a la version 7.28

--- database/migrations/2020_10_05_070850_crear_tabla_permiso_rol.php
+++ database/migrations/2020_10_05_070850_crear_tabla_permiso_rol.php
@@ -18,7 +18,7 @@ class CrearTablaPermisoRol extends Migration
             $table->unsignedInteger('rol_id');
             $table->foreign('rol_id', 'fk_permisorol_rol')->references('id')->on('rol')->onDelete('restrict')->onUpdate('restrict');
             $table->unsignedInteger('permiso_id');
-            $table->foreign('permiso_id', 'fk_permisorol_usuario')->references('id')->on('permiso')->onDelete('restrict')->onUpdate('restrict');
+            $table->foreign('permiso_id', 'fk_permisorol_permiso')->references('id')->on('permiso')->onDelete('restrict')->onUpdate('restrict');
             $table->timestamps();
         });
     }
